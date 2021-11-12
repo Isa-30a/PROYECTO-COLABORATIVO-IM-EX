d9257b4f07f2e4427daf5469ea7ee59044187fe9
ISA <isaalva3006@gmail.com>
2021-11-11 23:42:34 -0500

    espacios "case"

--- IM&EX cc.cpp
+++ IM&EX cc.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
     //variables de opciones
     int op,op2;
+    //variables vectores
+    int n;
+    cout<<"Ingresar numero de llamadas realizadas";cin>>n;
+    string nombres[n];
 
     cout<<"\tIM&EX SA."<<endl;
     cout<<"\tCONTROL DE LLAMADAS - CENTRO DE COMUNICACIONES"<<endl;
@@ -18,31 +22,31 @@ int main(){
     }while(op<1 && op>4);
 
     switch (op){
-    case 1://Registro de llamadas
-        
+        case 1://Registro de llamadas
+            
 
-        
-        break;
-    case 2:// Reportes / consultas
-        cout<<"1. Consulta por oficina"<<endl;
-        cout<<"2. Consulta por tipo de llamada"<<endl;
-        cout<<"3. Consulta llamadas por funcionario"<<endl;
-        cout<<"4. Reporte de llamadas"<<endl;
-        do{ 
-            cout<<"Ingrese la opcion: ";cin>>op2;
-        }while(op2<1 && op2>4);
+            
+            break;
+        case 2:// Reportes / consultas
+            cout<<"1. Consulta por oficina"<<endl;
+            cout<<"2. Consulta por tipo de llamada"<<endl;
+            cout<<"3. Consulta llamadas por funcionario"<<endl;
+            cout<<"4. Reporte de llamadas"<<endl;
+            do{ 
+                cout<<"Ingrese la opcion: ";cin>>op2;
+            }while(op2<1 && op2>4);
 
-        break;
-    case 3://Estadisticas
+            break;
+        case 3://Estadisticas
 
 
 
-        break;
-    default://Mesnsaje salida
+            break;
+        default://Mesnsaje salida
 
-        cout<<"Esta saliendo de IM&EX S.A"<<endl;
-        system("pause");
-        break;
+            cout<<"Esta saliendo de IM&EX S.A"<<endl;
+            system("pause");
+            break;
     }
 
 
