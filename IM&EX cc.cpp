ce276faf0b862c5cf2847d293424256610701490
ISA <isaalva3006@gmail.com>
2021-11-11 19:26:58 -0500

    Menu #1 (general)

--- IM&EX cc.cpp
+++ IM&EX cc.cpp
@@ -3,12 +3,37 @@
 using namespace std;
 
 int main(){
+    //variables de opciones
+    int op;
 
+    do{
+        cout<<"Ingrese la opcion: ";cin>>op;
+    }while(op<1 && op>4);
+    
+    switch (op)
 
+    {
+    case 1:
+        
+        
+        break;
+    case 2:
+    
+        break;
+    case 3:
 
-
-
-
-
-
+        break;
+    
+    
+    default:
+        cout<<"Esta saliendo de IM&EX"<<endl;
+        system("pause");
+        break;
     }
+
+
+
+
+
+
+}
